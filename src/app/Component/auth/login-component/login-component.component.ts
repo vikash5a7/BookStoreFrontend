da281296e0a45719963b7a0f95eb1526d2fc9b43
vikash kumar <vikashkumargupta5a7@gmail.com>
2020-06-02 12:08:43 +0530

    [vikash]add. toolbar addded and footer and dashboard design

--- src/app/Component/auth/login-component/login-component.component.ts
+++ src/app/Component/auth/login-component/login-component.component.ts
@@ -28,7 +28,7 @@ export class LoginComponentComponent implements OnInit {
               private matSnackBar: MatSnackBar,
               private titleService: Title
     ) {
-      this.setTitle('Fundoo Notes');
+      this.setTitle('Bookstore | Login');
   }
   handleError(error: { error: any; }) {
     this.isLoading = false;
--- src/app/Component/displaybook/displaybook.component.ts
+++ src/app/Component/displaybook/displaybook.component.ts
@@ -12,93 +12,88 @@ export class DisplaybookComponent implements OnInit {
 
 
   boo: any;
-  book=[];
+  book = [];
   books: BookModule = new BookModule();
   items = [];
-  pageofItems : Array<BookModule> = new Array<BookModule>();
-  obj : BookModule[];
+  pageofItems: Array<BookModule> = new Array<BookModule>();
+  obj: BookModule[];
   size: number;
-  book_id:number;
+  // tslint:disable-next-line: variable-name
+  book_id: number;
   bookSearch: any;
   bookName: string;
-  length : any = sessionStorage.length;
-  pageEvent : PageEvent;
+  length: any = sessionStorage.length;
+  pageEvent: PageEvent;
   lengths = 24;
   pageSize = 8;
-  data:any;
-  reminder:any;
-  s:any;selectoption:any;
-  value:any=[];
+  data: any;
+  reminder: any;
+  s: any; selectoption: any;
+  value: any = [];
   // @Output() output : EventEmitter<any> = new EventEmitter();
-leng : any;
-  constructor( private service : BookService, private snakbar : MatSnackBar) { }
- 
+leng: any;
+  constructor( private service: BookService, private snakbar: MatSnackBar) { }
+
   ngOnInit() {
-    
+    this.getallBooks();
     this.leng = sessionStorage.length;
     for (let i = 0; i < sessionStorage.length; i++) {
-      let key = sessionStorage.key(i);
+      const key = sessionStorage.key(i);
       this.value[sessionStorage.getItem(key)] = sessionStorage.getItem(key);
-      console.log("key ::" + key);
+      console.log('key ::' + key);
     }
     console.log(this.value);
-
-    
-    // this.getallBooks();
-    // this.items = Array(11).fill(0).map((x, i) => ( { array : this.book }));  
-    // this.addtobag();
-   
   }
 
   getallBooks() {
- 
+    console.log('gett all book called');
     this.service.getallBooks().subscribe( response => {
      this.book = response.bookList;
      this.obj = response.bookList;
      this.size = response.bookList.length;
      this.pageofItems = response.bookList;
-     console.log("Books ::::"+this.obj);
+     console.log('Books ::::' + this.obj);
      return this.book;
     });
     this.getSearchBookData();
   }
 
-  modo(value: string){
-    switch(value) {
-      case "mod1":
+  modo(value: string) {
+    switch (value) {
+      case 'mod1':
          this.sortingl();
          break;
-      case "mod2":
+      case 'mod2':
          this.sortingh();
          break;
-      case "mod3":
+      case 'mod3':
          this.SortNewestArrival();
          break;
     }
   }
 
-  sortingl(){
-    this.data=true;
+  sortingl() {
+    this.data = true;
     this.service.sorting(this.data).subscribe( response => {
       this.boo = response.bookList;
-     this.obj = response.bookList;
+      this.obj = response.bookList;
     //  this.size = response.bookList.length;
     //  this.pageofItems = response.bookList;
-    console.log(this.boo,"booklist"); 
+      console.log(this.boo, 'booklist');
    // console.log("Books ::::"+this.book);
 
       return this.boo;
     });
     this.getSearchBookData();
   }
-  sortingh(){
-    this.data=false;
+  sortingh() {
+    this.data = false;
     this.service.sorting(this.data).subscribe( response => {
       this.boo = response.bookList;
-     this.obj = response.bookList;
+      this.obj = response.bookList;
     //  this.size = response.bookList.length;
     //  this.pageofItems = response.bookList;
-    console.log(this.boo,"booklist"); 
+      console.log(this.boo, 'booklist');
    // console.log("Books ::::"+this.book);
 
       return this.boo;
@@ -106,19 +101,18 @@ leng : any;
     this.getSearchBookData();
   }
 
-    addtobag( bookId : any)
-    {
-  sessionStorage.setItem(bookId,bookId);
+    addtobag( bookId: any) {
+  sessionStorage.setItem(bookId, bookId);
   this.getOutput();
   this.ngOnInit();
 }
 
 getData(event?: PageEvent) {
-  console.log(" event:::"+event.pageIndex);
+  console.log(' event:::' + event.pageIndex);
   this.service.getPagination(event.pageIndex).subscribe( result => {
                  this.boo = result.bookList;
                  this.size = result.bookList.length;
-                 console.log(" data:::"+result.bookList);
+                 console.log(' data:::' + result.bookList);
   });
   this.getSearchBookData();
   return event;
@@ -132,22 +126,22 @@ getOutput() {
     this.ngOnInit();
     }
 
-onChangePage( pageofItems : Array<any>) {
+onChangePage( pageofItems: Array<any>) {
  this.pageofItems = pageofItems;
 }
 getSearchBookData() {
   this.service.getSearchBookData().subscribe((message) => {
-    console.log("search data", message.books);
+    console.log('search data', message.books);
     this.bookSearch = message.books;
   });
-  
-  
+
+
 }
-SortNewestArrival(){
-  this.service.SortNewestArrival().subscribe((response:any)=>{
+SortNewestArrival() {
+  this.service.SortNewestArrival().subscribe((response: any) => {
     this.boo = response.bookList;
-    console.log(this.boo,"booklist");
-      this.obj = response.bookList;
+    console.log(this.boo, 'booklist');
+    this.obj = response.bookList;
   });
   this.getSearchBookData();
 }
