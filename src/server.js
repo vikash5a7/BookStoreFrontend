01097c981fe90003a07ab895c65036056119c428
vikash kumar <vikashkumargupta5a7@gmail.com>
2020-06-24 16:13:09 +0530

    [vikash] json refactore

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/book-store'));
+app.use(express.static('./dist'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname +
-        '/dist/book-store/index.html'));
+    res.sendFile(__dirname + '/dist/index.html');
 });
 app.listen(process.env.PORT || 8080);
