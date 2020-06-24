c77955c8fbd7888c54f80ff9f8475453d2d104ed
vikash kumar <33521555+vikash5a7@users.noreply.github.com>
2020-06-24 14:35:42 +0530

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
 
 
   // BASE_URL: 'http://localhost:8081/',
